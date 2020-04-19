72e2a586118071dd8b8df18263397fd0fcbf04b8
amitsarna <amit.sarna.584@gmail.com>
2020-04-19 08:55:22 +0100

    Updates

---
+++ precache-manifest.cd5044530b940888e8485d8baec945b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300215900df6146811dd672b36604528",
+    "url": "/LetterDoorReact/index.html"
+  },
+  {
+    "revision": "471e0b3d79e315d43efe",
+    "url": "/LetterDoorReact/static/css/main.31b1894e.chunk.css"
+  },
+  {
+    "revision": "17caf79e6fbe51addd76",
+    "url": "/LetterDoorReact/static/js/2.be55cf20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LetterDoorReact/static/js/2.be55cf20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "471e0b3d79e315d43efe",
+    "url": "/LetterDoorReact/static/js/main.1083fdfe.chunk.js"
+  },
+  {
+    "revision": "97979e6a889ab59daa15",
+    "url": "/LetterDoorReact/static/js/runtime-main.fee4e46f.js"
+  }
+]);
